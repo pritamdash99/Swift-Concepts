4de8b626f0b0399106ff20288a5987fe1deaf414
Pritam Dash <pritamdash2015@outlook.com>
2022-06-04 22:04:17 +0530

    Initial Commit

---
+++ Swift_Basic_Concepts/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Swift_Basic_Concepts
+//
+//  Created by Pritam Dash on 04/06/22.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+
+
+//var swiftLoopsObj = SwiftLoops()
+//swiftLoopsObj.looping()
---
+++ Swift_Loops/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Swift_Loops
+//
+//  Created by Pritam Dash on 04/06/22.
+//
+
+import Foundation
+
+for _ in 0..<5 {
+    print("Hello World")
+}
+
