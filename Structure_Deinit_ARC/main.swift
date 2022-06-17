e16a89afb999513fff28b49b18e5998fad21a9e6
Pritam Dash <pritamdash2015@outlook.com>
2022-06-17 23:25:50 +0530

    Structures Deinit Arc code added

--- Structure_Deinit_ARC/main.swift
+++ Structure_Deinit_ARC/main.swift
@@ -25,4 +25,101 @@
  */
 import Foundation
 
+class Employee {
+    var empID : Int = 0
+    var salary : Float = 0
+    init(){
+        
+    }
+    init(empID : Int, salary : Float) {
+        self.empID = empID
+        self.salary = salary
+    }
+    deinit // executes just before object is deleted from memory.
+    {
+        print("Hello I'm going out from memory")
+    }
+}
+
+//-----------
+//Whenever more references of the same object is created the reference count increases by 1
+
+var e1 : Employee? = Employee(empID: 101, salary: 567000) // refernce count = 1
+print(e1!.empID, e1!.salary) // o/p : 101 567000.0
+
+var e2 : Employee? = e1 // refernce count = 2
+//Here e1(object)'s memory address gets assigned to e2. No copy of e1 is created. Pass by reference.
+
+e2?.salary = 453000
+//Thus by changing e2's salary e1's salary will get affected.
+print(e2!.salary, e1!.salary) // O/p : 453000.0 453000.0
+
+var e3 : Employee?
+e3 = e1     // refernce count = 3
+
+e3 = nil // refernce count = 2
+e1 = nil // refernce count = 1
+e2 = nil // refernce count = 0
+
+//now object is removed from memory.
+print(e1?.salary)   //If you force unwrap it then error will occur.
+
+//When all the references of the same object are deleted, only then the de-init() gets triggered.
+
+/*
+ Total o/p :
+ 101 567000.0
+ 453000.0 453000.0
+ Hello I'm going out from memory
+ nil
+ */
+
+//If you want to write something just before the object is deleted from the memory. You write it inside deinit{..}
+
+print("========= Structure ======")
+
+struct Student {
+    var rollno : Int = 0
+    var marks : Float = 0
+    var name : String = ""
+    
+    //Here we get default parameterised init()
+    func display() {
+        print(rollno, name, marks)
+    }
+}
+
+//Creating object of Student
+
+var s1 = Student(rollno: 101, marks: 56.7, name: "Pritam")
+s1.display()
+
+//pass by value or pass by refernce ?
+
+var s2 : Student? = s1  //Pass by value.
+//Here an entire copy of s1 is created and assigned to s2. So now 2 different memory allotment. Thus changing properties of s2 won't affect s1.
+
+s2?.marks = 78.2
+
+s1.display()    // 101 Pritam 56.7
+s2?.display()   // 101 Pritam 78.2
+
+// Here in structures no special management needs to be done manually so no need for de-init{..} in structures.
+//Every object of structure is an independent object.
+
+struct Person1 {
+    var age : Int = 0
+}
+
+protocol Person2 {
+    var age : Int {get set} //As only get set is mentioned the initialization must happen in the inherited struct.
+}
+
+//struct Driver : Person1{...} is not allowed as inheritance in structure is not possible.
+
+struct Driver : Person2{
+    var name : String = ""
+    var age: Int = 0
+}
+
 
