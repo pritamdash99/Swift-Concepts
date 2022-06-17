5b4fea9ffb741df5db0b041480178e664e74a3d7
Pritam Dash <pritamdash2015@outlook.com>
2022-06-17 23:29:09 +0530

    Notes Added

---
+++ Swift_Arc/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Swift_Arc
+//
+//  Created by Pritam Dash on 17/06/22.
+//
+/*
+ ARC - Automatic Reference Counting
+ This means swift deletes an unused object of a class from memory, it does it via ARC.
+ So qsn is does swift automatically does all these or how you write a code is also a factor in ARC.
+ 
+ */
+import Foundation
+
+
