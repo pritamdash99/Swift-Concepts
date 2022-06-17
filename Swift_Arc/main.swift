9977db9cbd2e6719decbdb4642066418cf990a55
Pritam Dash <pritamdash2015@outlook.com>
2022-06-17 23:33:04 +0530

    Example 3 added

--- Swift_Arc/main.swift
+++ Swift_Arc/main.swift
@@ -74,3 +74,65 @@ class Person {
 // name is :  codecat15
 // Deinit called for person class
 // */
+//Example 3 :
+
+class Job {
+    var jobDescription : String
+    var person : Person?
+    
+    init(_jobDescription : String)
+    {
+       jobDescription = _jobDescription
+        print("Init method for Job is called")
+    }
+    
+    deinit{
+        print("Deinit called for person class")
+    }
+}
+//
+//if(1==1)
+//{
+//    let objPerson = Person(_name: "codecat15")
+//    let objJob = Job(_jobDescription: "Swift Programmer")
+//    objPerson.job = objJob
+//}
+/*
+ O/p :
+ Init method for Person is called
+ Init method for Job is called
+ Deinit called for person class
+ Deinit called for person class
+ */
+
+if(1==1)
+{
+    let objPerson = Person(_name: "codecat15")
+    let objJob = Job(_jobDescription: "Swift Programmer")
+    objPerson.job = objJob
+    objJob.person = objPerson
+}
+/*
+ O/p :
+ Init method for Person is called
+ Init method for Job is called
+ 
+ So as you can see here the deint method is not called, because here "strong reference cycle" is being followed. Strong reference cycle means if ob1 is refering to ob2 and ob2 is also refering to ob1 then there is a cycle and both ob1 and ob2 keep using each other in cycle.
+ So swift doesnot delete these references from memory even if they are used just for a particular condition.
+ This is bad as the references stay in the memeory.
+ 
+ But as we do need both the objects to use each other but still not maintain the cycle, we need to mention to swift explicitly to remove the cycle.
+ We do that by by using the "weak" keyword before variable declaration of any class.
+ According to your usecase you can put up weak keyword in both classes.
+ 
+ 
+ Post weak keyword O/p :
+ Init method for Person is called
+ Init method for Job is called
+ Deinit called for person class
+ Deinit called for person class
+ 
+ A question about deinit : Can we add function, vars inside deiinit ?
+ Ans - Yes, you can if you a condition to be met before the references get dealocated.
+ 
+ */
