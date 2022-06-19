e571e89751b4fe09f9cff5a3e84916c16e0282d7
Pritam Dash <pritamdash2015@outlook.com>
2022-06-19 20:58:26 +0530

    Student Class Created.

--- InheritanceInSwift/main.swift
+++ InheritanceInSwift/main.swift
@@ -40,3 +40,36 @@ p1.display()
  Pritam 33
 */
 
+//If I wanted an app to store student details
+
+class Student : Person  //Student is inheriting Person
+{
+    var rollno : Int = 0
+    //Now this class has 3 variables post inheritance.
+    override init() {
+        super.init()
+        //When you try to modify the init of the child class then always 1st call the init() of super class then write child class init().
+        //You do this to initialise the variables declared in the super class.
+        print("I am student init")
+        self.age = 5
+    }
+    func displayData() -> Void {
+        print("Student -", rollno, name, age)
+    }
+}
+let s1 = Student()
+s1.rollno = 1101
+s1.name = "Rohan"
+s1.age = 22
+s1.display()
+/*
+ O/p :
+ I am Person default init
+ I am student init
+ Rohan 22
+ 
+ Thus with student object the default constructor of the Person class is also called when an object is formed of Student class because of Inheritance.
+ */
+
+s1.displayData()
+// O/p : Student - 1101 Rohan 22
