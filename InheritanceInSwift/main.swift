43f43434f52363fd83a9768d66c5b24eb95f3df2
Pritam Dash <pritamdash2015@outlook.com>
2022-06-19 20:55:50 +0530

    Note added.

---
+++ InheritanceInSwift/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  InheritanceInSwift
+//
+//  Created by Pritam Dash on 19/06/22.
+//
+
+import Foundation
+
+/*
+ Inheritance : Creating a new class by using code of existing class.
+ eg : Bike - Bajaj Pulsar 150 -> Bajaj Pulsar 220
+ Types of Inheritance -
+ 1. Single Inheritance - New class designed based on One existing Class : A -> B
+ 2. Multilevel : A -> B -> C
+ 3. Heirarchical : A -> X , A -> Y
+ 4. Hybrid : Mix any of the above type.
+*/
+
