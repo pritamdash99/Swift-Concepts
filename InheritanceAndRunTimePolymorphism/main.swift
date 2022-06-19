deca875e5e85c6c48039b5c38a938c83f4ee9c2e
Pritam Dash <pritamdash2015@outlook.com>
2022-06-19 21:05:44 +0530

    OneBHK class created.

--- InheritanceAndRunTimePolymorphism/main.swift
+++ InheritanceAndRunTimePolymorphism/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-
+class OneBHK {
+    var hall : Float = 0
+    var room : Float = 0
+    var kitchen : Float = 0
+    
+    func display() {
+        print("One BHK : ",room,hall,kitchen)
+    }
+    
+    //function to get area of flat.(sum of rooms of flat)
+    func getArea() -> Float {
+        return hall + room + kitchen
+    }
+}
 
