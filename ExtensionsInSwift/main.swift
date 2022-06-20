2746ca1c0a2665095c19539130d0e94ca1c53202
Pritam Dash <pritamdash2015@outlook.com>
2022-06-20 23:29:15 +0530

    Extension notes added.

--- ExtensionsInSwift/main.swift
+++ ExtensionsInSwift/main.swift
@@ -7,5 +7,5 @@
 
 import Foundation
 
-
+//Extension is a part of existing class, structure or enum to add new code(variables, functions, initializers) in a seperate declaration. i.e new features can be added to existing class, structure, enum without modifying the actual source code.
 
